ba9db82304176fa447a576afdf5e80ff86a80af7
Janaina Eufrasio <janainaeufrasio@hotmail.com>
2025-09-15 17:30:05 -0300

    Consulta de carros na Tabela FIPE

---
+++ src/main/java/br/com/alura/tabelafipe/model/Dados.java
@@ -0,0 +1,7 @@
+package br.com.alura.tabelafipe.model;
+
+import java.awt.*;
+
+public record Dados(String codigo, String nome) {
+
+}
---
+++ src/main/java/br/com/alura/tabelafipe/model/Modelos.java
@@ -0,0 +1,11 @@
+package br.com.alura.tabelafipe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.exc.IgnoredPropertyException;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Modelos(List<Dados> modelos) {
+
+}
